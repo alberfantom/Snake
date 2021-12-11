c6bc2ce6d241edacac97ba374d4cdfcc1bc62951
PrimenZzz <albertfantom11@gmail.com>
2021-12-11 14:06:00 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,14 +1,11 @@
 from field import *
 
 class Snake():
-    
-    def __init__(self,fild):
-
+    def __init__(self, field):
         self.coords = list()
-        self.coords.append([fild.size/2,fild.size/2])
+        self.coords.append([field.size // 2, field.size // 2])
 
-    def move(self,side):
-
+    def move(self, side):
         i = len(self.coords) - 1
         while i > 0:
             self.coords[i] = self.coords[i - 1]
@@ -16,13 +13,13 @@ class Snake():
             i -= 1
         
         if side == "b'w'":
-            self.coords[0][1] -= 1
-        elif side == "b's'":
-            self.coords[0][1] += 1
-        elif side == "b'a'":
             self.coords[0][0] -= 1
-        elif side == "b'd'":
+        elif side == "b's'":
             self.coords[0][0] += 1
+        elif side == "b'a'":
+            self.coords[0][1] -= 1
+        elif side == "b'd'":
+            self.coords[0][1] += 1
 
 
 
