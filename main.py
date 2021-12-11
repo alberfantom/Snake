9b96b668d856e57c29bc8718cbcab1d6f848e3fd
NeronSuper <byblikru228@gmail.com>
2021-12-11 11:40:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+#import time
+import msvcrt
+from apple import Apple
 from field import *
 from snake import *
-
 
 
 if __name__ == "__main__":
     pass
 
-_field = Field(10,10)
 
+_field = Field(10,10)
+_apple = Apple(_field)
 _snake = Snake(_field)
+
+
+direction = ''
+while 1:
+    #time.sleep(1)
+    if msvcrt.kbhit():
+        direction = format(msvcrt.getch())
+    
+    _snake.move(_snake,direction)
+
+
+    
