b770361259e0e82b4f574a557140dabe9a9f96fa
PrimenZzz <albertfantom11@gmail.com>
2021-12-11 12:37:27 +0300

    Update field.py

--- field.py
+++ field.py
@@ -6,13 +6,16 @@ class Field:
     
     # DECORATOR
 
-    def fill(self):
+    def fill(self, _snake, _apple):
         for row in range(self.size):
             for column in range(self.size):
-                self.field += "#"
+                if [row, column] in _apple.coord:
+                    self.field += "$"
+
+                elif [row, column] in _snake:
+                    self.field += "@"
+
+                else:
+                    self.field += "#" 
 
             self.field += "\n"
-            
-obj = Field(5)
-
-obj.fill()
