192ef3fdf4e0b4e2364c4959beffba16d58b9e85
PrimenZzz <albertfantom11@gmail.com>
2021-12-11 14:06:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-#import time
+from time import sleep
 import msvcrt
 from apple import *
 from field import *
 from snake import *
+import os
 
 
 if __name__ == "__main__":
@@ -13,16 +14,23 @@ _field = Field(10)
 _snake = Snake(_field)
 _apple = Apple(_field,_snake)
 
-
 direction = ''
-while 1:
-    #time.sleep(1)
+
+while True:
+    sleep(0.5)
+
     if msvcrt.kbhit():
         direction = format(msvcrt.getch())
     
     _snake.move(direction)
-    _field.fill(_snake,_apple)
-    print(_field.field)
+
+    # _field.show(_field.fill(_snake, _apple))
+    
+    _field.fill(_snake, _apple)
+
+    os.system("CLS")
+
+    _field.show()
 
 
     
