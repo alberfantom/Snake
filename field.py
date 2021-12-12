071a558eed093cdf505642c564be9b35fcfe6544
PrimenZzz <albertfantom11@gmail.com>
2021-12-12 13:28:32 +0300

    Update field.py

--- field.py
+++ field.py
@@ -1,11 +1,18 @@
 class Field:
-    def init(self, size):
+    def __init__(self, size):
         self.size = size
 
     def fill(self, _snake, _apple):
         self.field = str()
 
+        # Верхняя граница
+        self.field += f" #{'#' * (self.size - 1)}\n"
+
         for row in range(self.size):
+            # Левый край
+            self.field += "#"   
+
+            # Содержимое
             for column in range(self.size):
                 if [row, column] in _apple.coord:
                     self.field += "$"
@@ -15,14 +22,14 @@ class Field:
 
                 else:
                     self.field += "." 
+            
+            # Правый край
+            self.field += "#"
 
+            self.field += "\n"
 
-            if self.size != row:
-                self.field += "\n"
+        # Нижняя граница
+        self.field += f" #{'#' * (self.size - 1)}\n"
 
     def show(self):
-        print("#" * self.size)
-
-        print(self.field)
-
-        print("#" * self.size)
+        print(self.field, end="")
