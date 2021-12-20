d18828c41e9b60da104d8ba24edb7372f44c4d63
PrimenZzz <albertfantom11@gmail.com>
2021-12-20 10:02:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 import time
 import msvcrt
-from apple import *
-from field import *
-from snake import *
 
+import field
+import snake
+import apple
 
-if __name__ == "__main__":
-    pass
+field = field.Field(12)
+snake = snake.Snake()
+apple = apple.Apple()
 
-
-_field = Field(10)
-_snake = Snake(_field)
-_apple = Apple(_field,_snake)
-
-direction = ''
+direction = ""
 
 start_time_move = time.time()
 while True:
-    
-
     if msvcrt.kbhit():
         direction = format(msvcrt.getch().decode())
 
     if (time.time() - start_time_move) > 0.3:
         start_time_move = time.time()
-        _snake.move(direction,_apple,_field)
-        _field.fill(_snake, _apple)
-        _field.show()
+        snake.move(direction)
+        field.fill()
+        field.show()
    
 
         
