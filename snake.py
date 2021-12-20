fa7899e3fe5e45496a1d3c974a6730edd5114e62
PrimenZzz <albertfantom11@gmail.com>
2021-12-20 14:58:36 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,20 +1,17 @@
-import main
-
 class Snake():
-    def __init__(self):
+    def __init__(self, _field):
         self.coords = list()
-        self.coords.append([main.field.size // 2, main.field.size // 2])
+        self.coords.append([_field.size // 2, _field.size // 2])
 
-    def move(self, side, apple, field):
-        self.apple = apple
-
+    def move(self, side, _field):
         i = len(self.coords) - 1
+        
         while i > 0:
             self.coords[i] = self.coords[i - 1]
 
             i -= 1
 
-        if not self.is_collision_border():
+        if not self.is_collision_border(_field):
             if side == "w":
                 self.coords[0][0] -= 1
 
@@ -27,13 +24,13 @@ class Snake():
             elif side == "d":
                 self.coords[0][1] += 1
 
-    def is_collision_border(self):
+    def is_collision_border(self, _field):
         coord = self.coords[0]
 
         # Правая и левая граница
         # Верхняя и нижняя граница
-        if coord[0] == 0 or coord[0] == 10 \
-            or coord[1] == 0 or coord[1] == 10:
+        if coord[0] == 0 or coord[0] == _field.size \
+            or coord[1] == 0 or coord[1] == _field.size:
                 return True
         
         return False
