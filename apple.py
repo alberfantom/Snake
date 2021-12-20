2987ccfda74635e66b1545f8dfdebf581255b3e2
PrimenZzz <albertfantom11@gmail.com>
2021-12-20 10:02:09 +0300

    Update apple.py

--- apple.py
+++ apple.py
@@ -1,11 +1,12 @@
 from random import *
+import main
 
 class Apple():
-    def __init__(self, field, snake):
-        self.spawn_apple(snake, field)
+    def __init__(self):
+        self.spawn_apple()
         
-    def spawn_apple(self, snake, field):
-        self.coord = [randint(1, field.size - 1), randint(1, field.size - 1)]
+    def spawn_apple(self):
+        self.coord = [randint(1, main.field.size - 1), randint(1, main.field.size - 1)]
         
-        if self.coord in snake.coords:
+        if self.coord in main.snake.coords:
             self.spawn_apple()
