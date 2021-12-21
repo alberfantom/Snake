fd4c5b26d7858babfe574a3d84f42c8841b21ad8
PrimenZzz <albertfantom11@gmail.com>
2021-12-21 09:40:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ direction = ""
 
 start_time_move = time.time()
 
-_field = field.Field(25)
+_field = field.Field(int(input("Введите размер поля: ")))
 _snake = snake.Snake(_field)
 _apple = apple.Apple(_field, _snake)
 
@@ -17,7 +17,7 @@ while True:
     if msvcrt.kbhit():
         direction = format(msvcrt.getch().decode())
 
-    if (time.time() - start_time_move) > 0.3:
+    if (time.time() - start_time_move) > 0.10:
         start_time_move = time.time()
 
         _snake.move(direction, _field, _apple)
