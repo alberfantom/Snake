62bec9955d55a295c8e161d590ec22147876771f
PrimenZzz <albertfantom11@gmail.com>
2021-12-21 08:42:44 +0300

    Update apple.py

--- apple.py
+++ apple.py
@@ -5,7 +5,7 @@ class Apple():
         self.spawn_apple(_field, _snake)
 
     def spawn_apple(self, _field, _snake):
-        self.coord = [randint(1, _field.size - 1), randint(1, _field.size - 1)]
+        self.coord = [randint(1, _field.size - 2), randint(1, _field.size - 2)]
         
         if self.coord in _snake.coords:
-            self.spawn_apple()
+            self.spawn_apple(_field, _snake)
