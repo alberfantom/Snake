1259e83a7c2e81d5e889d9311f50d50ee692d802
PrimenZzz <albertfantom11@gmail.com>
2021-12-21 09:55:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,18 @@ _field = field.Field(int(input("Введите размер поля: ")))
 _snake = snake.Snake(_field)
 _apple = apple.Apple(_field, _snake)
 
+FPS = 2.5
+
 while True:
     if msvcrt.kbhit():
         direction = format(msvcrt.getch().decode())
 
-    if (time.time() - start_time_move) > 0.10:
-        start_time_move = time.time()
+    start_time_move = time.time()
 
-        _snake.move(direction, _field, _apple)
+    _snake.move(direction, _field, _apple)
 
-        _field.fill(_snake, _apple)
-        _field.show()
+    _field.fill(_snake, _apple)
+    _field.show()
 
+    time.sleep(1 / FPS)
         
