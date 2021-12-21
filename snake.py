833b9751748cb0cc96780d03e381ee24a85cf27d
PrimenZzz <albertfantom11@gmail.com>
2021-12-21 08:42:49 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -4,48 +4,42 @@ class Snake():
         self.coords.append([_field.size // 2, _field.size // 2])
 
     def move(self, side, _field, _apple):
-        if len(self.coords) != 1:
-            i = len(self.coords) - 1
-
-            while i > 0: 
-                self.coords[i][0] = self.coords[i - 1][0]
-                self.coords[i][1] = self.coords[i - 1][1]
-
-                i -= 1
-
         if not self.is_collision_border(_field):
+            if len(self.coords) != 1:
+                i = len(self.coords) - 1
+
+                while i > 0: 
+                    self.coords[i][0] = self.coords[i - 1][0]
+                    self.coords[i][1] = self.coords[i - 1][1]
+
+                    i -= 1
+
             coord_basic = [self.coords[-1][0], self.coords[-1][1]]
-
-            # x, y - стоят не правильно
-
-            print(self.coords)
-
+            
             if side == "w":
-                self.coords[0][0] -= 1
-
-            elif side == "s":
-                self.coords[0][0] += 1
-
-            elif side == "a":
                 self.coords[0][1] -= 1
 
-            elif side == "d":
+            elif side == "s":
                 self.coords[0][1] += 1
 
-            print(self.coords)
-            
-        if self.is_collision_apple(_apple):
-            self.coords.append(coord_basic)
+            elif side == "a":
+                self.coords[0][0] -= 1
 
-            _apple.spawn_apple(_field, self)
+            elif side == "d":
+                self.coords[0][0] += 1
+                
+            if self.is_collision_apple(_apple):
+                self.coords.append(coord_basic)
+
+                _apple.spawn_apple(_field, self)
 
     def is_collision_border(self, _field):
         coord = self.coords[0]
 
         # Правая и левая граница
         # Верхняя и нижняя граница
-        if coord[0] == 0 or coord[0] == _field.size \
-            or coord[1] == 0 or coord[1] == _field.size:
+        if coord[0] == 0 or coord[0] == _field.size - 1\
+            or coord[1] == 0 or coord[1] == _field.size - 1:
                 return True
         
         return False
